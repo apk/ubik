89035ee5894255b8462f5b4cd73fb881f453846b
Andreas Krey <a.krey@gmx.de>
2017-10-02 20:07:19 +0200

    build.sh: run in containing dir

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 
 
+cd "`dirname "$0"`"
+
 # run-on-change ubik.c jfdt/.obj/jfdt.a jfdt/h/jfdt/*.h -- gcc -o ubik -I jfdt/h main.c jfdt/.obj/jfdt.a -- ./ubik /usr/bin/sleep 15 ---- /usr/bin/sleep 18 ----3 /usr/bin/echo adf ----2 /usr/bin/sleep 
 
 test -d jfdt || git clone https://github.com/apk/jfdt.git || exit 7
