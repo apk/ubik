da3c8a4c6b68ec7c3a488bca667116e1cf543a6c
Andreas Krey <a.krey@gmx.de>
2017-10-03 21:29:20 +0200

    build.sh: try ../jfdt as source, too

--- build.sh
+++ build.sh
@@ -4,8 +4,17 @@ cd "`dirname "$0"`"
 
 # run-on-change ubik.c jfdt/.obj/jfdt.a jfdt/h/jfdt/*.h -- gcc -o ubik -I jfdt/h main.c jfdt/.obj/jfdt.a -- ./ubik /usr/bin/sleep 15 ---- /usr/bin/sleep 18 ----3 /usr/bin/echo adf ----2 /usr/bin/sleep 
 
-test -d jfdt || git clone https://github.com/apk/jfdt.git || exit 7
+jfdt=jfdt
+if test -d "$jfdt"; then
+    : nix
+elif test -d ../jfdt; then
+    jfdt=../jfdt
+else
+    git clone https://github.com/apk/jfdt.git || exit 7
+fi
 
-sh jfdt/the.sh || exit 5
+set -x
 
-gcc -o ubik -I jfdt/h main.c jfdt/.obj/jfdt.a || exit 3
+sh "$jfdt"/the.sh || exit 5
+
+gcc -o ubik -I "$jfdt"/h main.c "$jfdt"/.obj/jfdt.a || exit 3
